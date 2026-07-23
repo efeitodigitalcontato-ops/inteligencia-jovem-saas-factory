49f4302822dbc1297083999aca257d6f499744ff
Efeito Digital Corp <efeitodigitalcontato@gmail.com>
2026-07-23 15:37:02 +0200

    Fix login button click and prevent JS runtime errors

--- server.js
+++ server.js
@@ -3382,7 +3382,7 @@ app.post('/api/login', async (req, res) => {
 
     res.json({
       success: true,
-      token: authData.session.access_token,
+      token: (authData.session && authData.session.access_token) ? authData.session.access_token : (authData.user ? authData.user.id : ''),
       user: {
         name: profile.name,
         email: profile.email,
